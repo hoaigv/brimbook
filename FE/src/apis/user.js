be726deff54aec01edaa04e0309d2e303bb75b38
NPG-ThaoVo <45417567+NPG-ThaoVo@users.noreply.github.com>
2024-09-24 15:00:44 +0700

    thao validate user token

--- FE/src/apis/user.js
+++ FE/src/apis/user.js
@@ -2,13 +2,15 @@ import axios from "axios";
 import { BASE_URL } from "~/utils/constants";
 
 export const loginUser = async (user, navigate) => {
-  await axios
-    .post(`${BASE_URL}/auth/login`, user)
-    .then((res) => res.data)
-    .then(() => {
+  try {
+    const response = await axios.post(`${BASE_URL}/auth/login`, user);
+    if (response.data && response.data.result.token) {
+      localStorage.setItem("userToken", response.data.result.token);
       navigate("/");
-    })
-    .catch((err) => console.log(err));
+    }
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const registerUser = async (user, navigate) => {
@@ -16,7 +18,12 @@ export const registerUser = async (user, navigate) => {
     .post(`${BASE_URL}/api/users/sign-up`, user)
     .then((res) => res.data)
     .then(() => {
-      navigate("/");
+      navigate("/login");
     })
     .catch((err) => console.log(err));
+};
+
+export const logoutUser = (navigate) => {
+  localStorage.removeItem("userToken");
+  navigate("/login");
 };
