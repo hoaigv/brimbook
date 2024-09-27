108c367396189b166b7252edae4f15936398398f
NPG-ThaoVo <45417567+NPG-ThaoVo@users.noreply.github.com>
2024-09-27 19:51:57 +0700

    update date loading page

--- FE/src/apis/http.js
+++ FE/src/apis/http.js
@@ -10,7 +10,11 @@ export const axios = Axios.create({
 
 axios.interceptors.request.use((config) => {
   const token = localStorage.getItem("userToken");
-  if (!config.url.includes("api/users/sign-up") && !config.url.includes("auth/login")) {
+  if (
+    !config.url.includes("api/users/sign-up") &&
+    !config.url.includes("auth/login") &&
+    !config.url.includes("api/books/getAll")
+  ) {
     config.headers = { ...config.headers, Authorization: `Bearer ${token}` };
   }
 
